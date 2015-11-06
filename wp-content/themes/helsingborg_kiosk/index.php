ea326460ee2eb5b818b1c7d70aa837ce9bb8fb95
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-06 14:17:13 +0100

    Metro grid

--- wp-content/themes/helsingborg_kiosk/index.php
+++ wp-content/themes/helsingborg_kiosk/index.php
@@ -1,3 +1,64 @@
 <?php get_header(); ?>
 
+<section id="content">
+    <h1 id="content-headline">Vad vill du göra idag?</h1>
+    <ul class="metro-grid">
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-pizza"></i>
+                    Restauranger
+                </div>
+            </a>
+        </li>
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-pizza"></i>
+                    Badplatser
+                </div>
+            </a>
+        </li>
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-ios-football"></i>
+                    Sport och idrott
+                </div>
+            </a>
+        </li>
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-pizza"></i>
+                    Museer
+                </div>
+            </a>
+        </li>
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-pizza"></i>
+                    Barn
+                </div>
+            </a>
+        </li>
+
+        <li class="metro-grid-item">
+            <a href="#">
+                <div class="metro-grid-item-content">
+                    <i class="ion-pizza"></i>
+                    Park/trädgård
+                </div>
+            </a>
+        </li>
+
+    </ul>
+</section>
+
 <?php get_footer(); ?>
