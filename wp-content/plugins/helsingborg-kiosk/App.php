b354933016fcd76f4e5cabe4847a3345ebbc7789
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-10 11:38:05 +0100

    Allow SVG uploads

--- wp-content/plugins/helsingborg-kiosk/App.php
+++ wp-content/plugins/helsingborg-kiosk/App.php
@@ -6,7 +6,15 @@ class App
 {
     public function __construct()
     {
+        add_filter('upload_mimes', array($this, 'allowSvg'));
+
         new PointOfInterest\CustomPostType();
         new Screensaver\Screensaver();
+    }
+
+    public function allowSvg($mimes)
+    {
+        $mimes['svg'] = 'image/svg+xml';
+        return $mimes;
     }
 }
