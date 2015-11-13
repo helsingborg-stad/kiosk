039d15fb1a9abd33304eb7e18ec67fdd2a3aa181
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-13 08:20:25 +0100

    Add is_null check on saved_kiosk

--- wp-content/themes/helsingborg_kiosk/lib/classes/places.class.php
+++ wp-content/themes/helsingborg_kiosk/lib/classes/places.class.php
@@ -1,122 +1,123 @@
-<?php 
-	
+<?php
+
 	namespace HelsingborgKiosk\Locations;
-	
+
 	Class HelsingborgKioskLocation {
-		
-		public static $kiosk_ip; 
-		
+
+		public static $kiosk_ip;
+
 		function __construct() {
-			
+
 			//ID
-			self::$kiosk_ip = $_SERVER['REMOTE_ADDR']; 
-			
-			//Hooks 
+			self::$kiosk_ip = $_SERVER['REMOTE_ADDR'];
+
+			//Hooks
 			add_action('init', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::register_options_page');
-			
+
 			//Ajax
 			add_action( 'wp_ajax_get_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::get_kiosk_location' );
-			add_action( 'wp_ajax_nopriv_get_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::get_kiosk_location' ); 
-			
+			add_action( 'wp_ajax_nopriv_get_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::get_kiosk_location' );
+
 			add_action( 'wp_ajax_set_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::set_kiosk_location' );
-			add_action( 'wp_ajax_nopriv_set_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::set_kiosk_location' ); 
-			
+			add_action( 'wp_ajax_nopriv_set_kiosk_location', 'HelsingborgKiosk\Locations\HelsingborgKioskLocation::set_kiosk_location' );
+
 		}
-		
+
 		public static function set_kiosk_location () {
-			
+
 			//Data
 			if (!isset($_POST['latitude'])||!isset($_POST['longitude'])) {
 				echo json_encode(array('json_result'=>false));
 				die();
 			} else {
-				$latitude 	= htmlspecialchars ( $_POST['latitude'] 	); 
-				$longitude 	= htmlspecialchars ( $_POST['longitude'] 	); 
+				$latitude 	= htmlspecialchars ( $_POST['latitude'] 	);
+				$longitude 	= htmlspecialchars ( $_POST['longitude'] 	);
 			}
-			
+
 			//Definitions
 			$has_kiosk = false;
-			
-			//Get saved data 
+
+			//Get saved data
 			$saved_kiosks =  get_field('kiosk_repeater', 'option');
-			
-			//Look for kiosk 
-			if ( !empty( $saved_kiosks ) ) { 
+
+			//Look for kiosk
+			if ( !empty( $saved_kiosks ) ) {
 				foreach ( $saved_kiosks as $saved_kiosk ) {
 					if ($saved_kiosk['kiosk_ip']==self::$kiosk_ip) {
-						$has_kiosk = true; 
+						$has_kiosk = true;
 						break;
-					} 
+					}
 				}
 			}
-			
+
 			//Not set, save  new
 			if ( $has_kiosk == false ) {
-				
+
 				if ( !is_array( $saved_kiosks ) ) {
 					$saved_kiosks = array();
 				}
-							
+
 				//Add missing kiosk
-				$saved_kiosks[] = array('kiosk_ip' => self::$kiosk_ip, 'kiosk_lat' => $latitude, 'kiosk_long' => $longitude ); 
-				
+				$saved_kiosks[] = array('kiosk_ip' => self::$kiosk_ip, 'kiosk_lat' => $latitude, 'kiosk_long' => $longitude );
+
 				//Update all
 				foreach ( $saved_kiosks as $kiosk_location_id => $kiosk_location ) {
 					foreach ( $kiosk_location as $key => $value ) {
 						update_sub_field( array('kiosk_repeater', ($kiosk_location_id+1), $key), $value );
-					} 
+					}
 				}
-				
+
 				//Update count
-				update_option('options_kiosk_repeater', count($saved_kiosks)); 
-				
+				update_option('options_kiosk_repeater', count($saved_kiosks));
+
 			}
-			
-			//Result 
-			echo json_encode(array('json_result'=>true)); 
-			die(); 
-			
+
+			//Result
+			echo json_encode(array('json_result'=>true));
+			die();
+
 		}
-		
+
 		public static function get_kiosk_location () {
-			
-			$saved_kiosks =  get_field('kiosk_repeater', 'option'); 
-			
-			//Look for kiosk 
-			
-			if ( !empty( $saved_kiosks ) ) { 
-				
+
+			$saved_kiosks =  get_field('kiosk_repeater', 'option');
+
+			//Look for kiosk
+			//
+
+			if ( !empty( $saved_kiosks ) && !is_null($saved_kiosks)) {
+
 				foreach ( $saved_kiosks as $saved_kiosk ) {
-					
+
 					if( $saved_kiosk['kiosk_ip'] == $_SERVER['REMOTE_ADDR'] ) {
-						$saved_kiosk['json_result'] = true; 
-						
+						$saved_kiosk['json_result'] = true;
+
 						if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-							echo json_encode( $saved_kiosk ); 
-							die(); 
+							echo json_encode( $saved_kiosk );
+							die();
 						} else {
-							return json_encode( $saved_kiosk ); 
+							return json_encode( $saved_kiosk );
 						}
 					}
-					
+
 				}
-			
+
 			}
-			
-			//No kiosk saved 
+
+			//No kiosk saved
 			if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-				echo json_encode( array('json_result' => false ) ); 
-				die(); 
+				echo json_encode( array('json_result' => false ) );
+				die();
 			} else {
-				return json_encode( array('json_result' => false ) ); 
+				return json_encode( array('json_result' => false ) );
 			}
-			
+
 		}
-		
+
 		public static function register_options_page () {
-			
+
 			if( function_exists('acf_add_options_page') ) {
-	
+
 				acf_add_options_page(array(
 					'page_title' 	=> 'Platser för kiosker',
 					'menu_title'	=> 'Kiosker',
@@ -124,8 +125,8 @@
 					'capability'	=> 'edit_posts',
 					'redirect'		=> false
 				));
-				
-				
+
+
 				if( function_exists('acf_add_local_field_group') ):
 
 				acf_add_local_field_group(array (
@@ -145,8 +146,8 @@
 								'class' => '',
 								'id' => '',
 							),
-							'message' => 'Här listas platserna för samtliga kiosker som finns i installationen. Varje kiosk uppdaterar automatiskt sin plats vid första besöket. Om en kiosk skulle hamna fel på kartan, kan du ändra den här. Tar du bort en kiosk, kommer den att dyka upp här igen när den används första gången. 
-				
+							'message' => 'Här listas platserna för samtliga kiosker som finns i installationen. Varje kiosk uppdaterar automatiskt sin plats vid första besöket. Om en kiosk skulle hamna fel på kartan, kan du ändra den här. Tar du bort en kiosk, kommer den att dyka upp här igen när den används första gången.
+
 				Samtliga kiosker kräver att en position finns lagrad för att plotta ut en vägbeskrivning till målet.',
 							'new_lines' => 'wpautop',
 							'esc_html' => 1,
@@ -278,45 +279,44 @@
 					'active' => 1,
 					'description' => '',
 				));
-				
+
 				endif;
 
 			}
-		
+
 		}
-		
+
 		public static function distance_kiosk($target_lat, $target_long ) {
 
-			//Convert to floats 
-			$target_lat 	= floatval( str_replace(",", ".", $target_lat ) ); 
-			$target_long 	= floatval( str_replace(",", ".", $target_long ) );  
-			
+			//Convert to floats
+			$target_lat 	= floatval( str_replace(",", ".", $target_lat ) );
+			$target_long 	= floatval( str_replace(",", ".", $target_long ) );
+
 			//Get val
-			$kiosk_data = json_decode( self::get_kiosk_location() );  
-			
+			$kiosk_data = json_decode( self::get_kiosk_location() );
+
 			if ( $kiosk_data->json_result == true ) {
-				
-				//Origin adssignments 
-				$origin_lat 	= floatval( str_replace(",", ".", $kiosk_data->kiosk_lat ) ); 
-				$origin_long 	= floatval( str_replace(",", ".", $kiosk_data->kiosk_long ) ); 
-			
-				//Calc degrees 
+
+				//Origin adssignments
+				$origin_lat 	= floatval( str_replace(",", ".", $kiosk_data->kiosk_lat ) );
+				$origin_long 	= floatval( str_replace(",", ".", $kiosk_data->kiosk_long ) );
+
+				//Calc degrees
 				$degrees = rad2deg(acos((sin(deg2rad($origin_lat))*sin(deg2rad($target_lat))) + (cos(deg2rad($origin_lat))*cos(deg2rad($target_lat))*cos(deg2rad($target_long-$target_long)))));
-				
+
 				//Convert to KM
 				return $degrees * 60 * 1.609344 * 1.7;
-				
+
 			} else {
-				return false; 
+				return false;
 			}
-			
+
 		}
-		
+
 		function __destruct() {
-			
+
 		}
-		
+
  	}
- 	
- 	new HelsingborgKioskLocation(); 
- 	
+
+ 	new HelsingborgKioskLocation();
