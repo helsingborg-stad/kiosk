afb32f08e1449f1184a1165296aaae6cf42909f6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-17 13:30:05 +0100

    Instagram base

--- wp-content/plugins/helsingborg-social-media/source/php/Curl/Instagram.php
+++ wp-content/plugins/helsingborg-social-media/source/php/Curl/Instagram.php
@@ -4,8 +4,77 @@ namespace HbgSocialMedia\Curl;
 
 class Instagram
 {
+    /**
+     * Api keys and secrets
+     * @var $key      Api key
+     * @var $secret   Api secret
+     */
+    private $key = null;
+    private $secret = null;
+
     public function __construct()
     {
 
+    }
+
+    public function auth()
+    {
+
+    }
+
+    /**
+     * Gets a user's feed
+     * @param  string $username The username of the user to get feed from
+     * @return object           The feed object
+     */
+    public function getUserFeed($username)
+    {
+        // Try to find the user id of the user with the given username
+        $endpoint = 'https://api.instagram.com/v1/users/search';
+        $data = array(
+            'q' => $username,
+            'client_id' => $this->key
+        );
+
+        $users = HbgCurl::request('GET', $endpoint, $data);
+        $users = json_decode($users);
+
+        $userId = null;
+
+        foreach ($users->data as $user) {
+            if ($user->username == $username) {
+                $userId = $user->id;
+                break;
+            }
+        }
+
+        // Get the feed
+        $endpoint = 'https://api.instagram.com/v1/users/' . $userId . '/media/recent/';
+        $params = array(
+            'client_id' => $this->key
+        );
+
+        $result = HbgSocialMedia\Helper\Curl::request('GET', $endpoint, $params);
+        $result = json_decode($result);
+
+        return $result->data;
+    }
+
+    /**
+     * Get a hashtag feed
+     * @param  string $hashtag The hashtag to get
+     * @return object          The hastag recent media object
+     */
+    public function getHashtag($hashtag)
+    {
+        $endpoint = 'https://api.instagram.com/v1/tags/' . $hashtag . '/media/recent';
+        $params = array(
+            'client_id' => $this->key
+        );
+
+        $result = HbgSocialMedia\Helper\Curl::request('GET', $endpoint, $params);
+        $result = json_decode($result);
+
+        return $result->data;
     }
 }
