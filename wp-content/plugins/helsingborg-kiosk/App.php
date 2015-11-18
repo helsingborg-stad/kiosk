52ca5fccfc63b35533bf4903a7fb89c62e9d4d14
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-11-18 11:09:47 +0100

    Made cronjob from CBIS import

--- wp-content/plugins/helsingborg-kiosk/App.php
+++ wp-content/plugins/helsingborg-kiosk/App.php
@@ -11,6 +11,14 @@ class App
         new PointOfInterest\CustomPostType();
         new Event\CustomPostType();
         new Screensaver\Screensaver();
+        
+        //Cron stuff
+        register_activation_hook(__FILE__, array($this,'add_cron_job'));
+        register_deactivation_hook(__FILE__, array($this,'remove_cron_job'));
+        
+        //Register hook 
+        add_action('import_cbis_event_hourly', array($this,'do_import_cbis_event'));
+        
     }
 
     public function allowSvg($mimes)
@@ -18,4 +26,17 @@ class App
         $mimes['svg'] = 'image/svg+xml';
         return $mimes;
     }
+
+	public function add_cron_job () {
+		wp_schedule_event(time(), 'hourly', 'import_cbis_event_hourly');
+	}
+	
+	public function remove_cron_job () {
+		wp_clear_scheduled_hook('import_cbis_event_hourly');
+	}
+	
+	public function do_import_cbis_event () {
+		new ParseCbis('http://familjenhelsingborg.se/cbisexport.csv');
+	}
+	
 }
