fc05db3b339a196ec18ef741dbf1983b2bb8d7c2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-18 11:22:55 +0100

    ACF fields

--- wp-content/plugins/helsingborg-kiosk/helsingborg-kiosk.php
+++ wp-content/plugins/helsingborg-kiosk/helsingborg-kiosk.php
@@ -17,7 +17,8 @@ require_once HBG_KIOSK_PATH . 'public.php';
 // Custom fields
 require_once HBG_KIOSK_PATH . 'source/acf/PoiCustomPostType.php';
 require_once HBG_KIOSK_PATH . 'source/acf/PoiCategory.php';
-//require_once HBG_KIOSK_PATH . 'source/acf/Screensaver.php';
+require_once HBG_KIOSK_PATH . 'source/acf/Screensaver.php';
+require_once HBG_KIOSK_PATH . 'source/acf/Takeover.php';
 
 // Instantiate and register the autoloader
 $loader = new HbgKiosk\Vendor\Psr4ClassLoader();
